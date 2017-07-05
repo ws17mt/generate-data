94aba22ca8f4b2ec81a344e989595d028c65acab
Gaurav Kumar <gaurav.bison@gmail.com>
2017-07-05 15:55:48 -0400

    Moved cleaning of training corpus to shuf_split_train

--- shuf_split_train.sh
+++ shuf_split_train.sh
@@ -10,6 +10,9 @@ CORPUS_2=/pylon2/ci560op/fosterg/data/fr/raw-data/news-commentary-v9.fr-en
 S_EXT=fr
 T_EXT=en
 
+MAX_SENT_LEN=80
+
+TOOLS_DIR=tools
 OUTDIR=data/fr_train
 
 mkdir -p $OUTDIR
@@ -17,12 +20,17 @@ mkdir -p $OUTDIR
 cat ${CORPUS_1}.${S_EXT} ${CORPUS_2}.${S_EXT} > ${OUTDIR}/train.raw.${S_EXT}
 cat ${CORPUS_1}.${T_EXT} ${CORPUS_2}.${T_EXT} > ${OUTDIR}/train.raw.${T_EXT}
 
+# Clean (truncate max length)
+echo "*** Cleaning corpus.${l}"
+${TOOLS_DIR}/clean-corpus-n.perl ${OUTDIR}/train.raw \
+  ${S_EXT} ${T_EXT} ${OUTDIR}/train.clean 1 $MAX_SENT_LEN
+
 # Concatenate files
-paste ${OUTDIR}/train.raw.${S_EXT} ${OUTDIR}/train.raw.${T_EXT} > ${OUTDIR}/train.raw.${S_EXT}${T_EXT}
+paste ${OUTDIR}/train.clean.${S_EXT} ${OUTDIR}/train.clean.${T_EXT} > ${OUTDIR}/train.clean.${S_EXT}${T_EXT}
 
 # Shuffle
 echo "*** Shuffling"
-cat ${OUTDIR}/train.raw.${S_EXT}${T_EXT} | shuf > ${OUTDIR}/train.shuf.${S_EXT}${T_EXT}
+cat ${OUTDIR}/train.clean.${S_EXT}${T_EXT} | shuf > ${OUTDIR}/train.shuf.${S_EXT}${T_EXT}
 
 # Split concatenated files
 cut -f1 ${OUTDIR}/train.shuf.${S_EXT}${T_EXT} > ${OUTDIR}/train.fr
@@ -41,4 +49,5 @@ head -n1000000 ${OUTDIR}/train.en > ${OUTDIR}/train.1m.en
 
 echo "*** Done. Cleaning up"
 rm ${OUTDIR}/train.raw.*
+rm ${OUTDIR}/train.clean.*
 rm ${OUTDIR}/train.shuf.${S_EXT}${T_EXT}
