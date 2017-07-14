d5ab2a46956ac6842b8cdb75d0a3b3b96aa965a9
Daniel Beck <daniel.e.beck@gmail.com>
2017-07-14 16:52:35 -0400

    oops, forgot to add the fr folder

---
+++ fr/preprocess_para.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# script to preprocess parallel training and dev data
+# note that the monolingual training data is preprocessed separately
+# for training and dev, tokenize, truecase, and apply BPE
+# for training only, strip empty and long sentences
+
+# relevant directories
+scripts=tools
+indata=/pylon2/ci560op/gkumar6/code/generate-data/data/fr_train
+indev=/pylon2/ci560op/gkumar6/code/generate-data/data/fr_dev
+outdata=/pylon2/ci560op/fosterg/data/fr/processed
+
+# models
+bpe_model=/pylon2/ci560op/gkumar6/code/generate-data/model/fr/fren.30000.bpe
+tc_model=/pylon2/ci560op/gkumar6/code/generate-data/model/fr/truecase-model
+
+# vars
+src=en
+trg=fr
+
+# for each data set size (10k, 100k, 1m, 2.2m, full)
+for size in 10k 100k 1m 2m full
+ do 
+  # preprocess
+  echo "Preprocessing $size data"
+  $scripts/preprocess.sh $indata/train.$size $outdata/train.$size $src $trg $tc_model
+  # apply BPE
+  echo "Applying BPE (mono 30k)"
+  $scripts/apply_bpe.sh $bpe_model $outdata/train.$size.tc.$src $outdata/train.$size.bpe.$src
+  $scripts/apply_bpe.sh $bpe_model $outdata/train.$size.tc.$trg $outdata/train.$size.bpe.$trg
+ done
+
+# for each of dev1, dev2, and test1
+for prefix in dev1 dev2 test1
+ do 
+  # preprocess (note can't use preprocessing script because we don't want to cut long sentences)
+  echo "tokenizing $prefix"
+  $scripts/tokenizer.perl -a -l $src -threads 12 <$indev/$prefix.$src >$outdata/$prefix.tok.$src
+  $scripts/tokenizer.perl -a -l $trg -threads 12 <$indev/$prefix.$trg >$outdata/$prefix.tok.$trg
+  echo "truecasing $prefix"
+  $scripts/truecase.perl -model $tc_model.$src <$outdata/$prefix.tok.$src >$outdata/$prefix.tc.$src
+  $scripts/truecase.perl -model $tc_model.$trg <$outdata/$prefix.tok.$trg >$outdata/$prefix.tc.$trg
+  # apply BPE
+  echo "applying BPE (mono 30k) to $prefix"
+  $scripts/apply_bpe.sh $bpe_model $outdata/$prefix.tc.$src $outdata/$prefix.bpe.$src
+  $scripts/apply_bpe.sh $bpe_model $outdata/$prefix.tc.$trg $outdata/$prefix.bpe.$trg
+ done
---
+++ fr/process_mono.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+# This script processes the raw monolingual datasets for WMT fr and en.
+# We use this learn truecasing and BPE models that can applied to 
+# parallel dataset of varying sizes.
+# The BPE model is learnt jointly with with two mono corpora
+# NOTE: Clean corpus (max-length) is not applied to mono datasets.
+
+RAW_DATA_DIR=/pylon2/ci560op/fosterg/data/fr/raw-data
+PROC_DATA_DIR=data/fr
+TOOLS_DIR=tools
+MODEL_DIR=model/fr
+
+##############################################
+# Max BPE splits
+BPE_OPERATIONS=30000
+# The monolingual datasets
+MONO="news.2007-13"
+THREADS_PER_L=12
+
+##############################################
+
+mkdir -p ${PROC_DATA_DIR}
+mkdir -p ${MODEL_DIR}
+
+##############################################
+
+stage=1
+
+if [[ $stage -lt 1 ]]; then
+  for l in fr en; do
+    (
+    # Tokenize
+    echo "*** Tokenizing mono.${l}"
+    ${TOOLS_DIR}/tokenizer.perl -a -l $l -threads ${THREADS_PER_L}\
+      < ${RAW_DATA_DIR}/${MONO}.${l} \
+      > ${PROC_DATA_DIR}/mono.tok.${l}
+
+    echo "*** Learning truecasing model for mono.${l}"
+    # Truecaser (train)
+    ${TOOLS_DIR}/train-truecaser.perl \
+      -corpus ${PROC_DATA_DIR}/mono.tok.${l} \
+      -model ${MODEL_DIR}/truecase-model.${l}
+
+    echo "*** Applying truecasing model to mono.${l}"
+    # Truecaser (apply)
+    ${TOOLS_DIR}/truecase.perl \
+      -model ${MODEL_DIR}/truecase-model.${l} \
+      < ${PROC_DATA_DIR}/mono.tok.${l} \
+      > ${PROC_DATA_DIR}/mono.tc.${l}
+    )&
+  done
+
+  wait;
+fi
+
+rm ${MODEL_DIR}/fren.${BPE_OPERATIONS}.bpe
+
+echo "*** Learning BPE model"
+# BPE (train)
+cat ${PROC_DATA_DIR}/mono.tc.fr ${PROC_DATA_DIR}/mono.tc.en \
+  | ${TOOLS_DIR}/learn_bpe.py -s ${BPE_OPERATIONS} > ${MODEL_DIR}/fren.${BPE_OPERATIONS}.bpe
+
+for l in fr en; do
+  echo "*** Applying BPE to mono.${l}"
+  # BPE (apply)
+  ${TOOLS_DIR}/apply_bpe.py -c ${MODEL_DIR}/fren.${BPE_OPERATIONS}.bpe \
+    < ${PROC_DATA_DIR}/mono.tc.${l} \
+    > ${PROC_DATA_DIR}/mono.bpe.${l}
+done
